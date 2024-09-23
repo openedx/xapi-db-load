8e7c8e35fc892ae9e7e7fae60a3dff34b603ef55
Brian Mesick <bmesick@tcril.org>
2024-09-23 13:09:03 -0400

    fix: Review feedback
    Fix typo, default tag parent_id to 0 instead of null.

--- xapi_db_load/backends/clickhouse_lake.py
+++ xapi_db_load/backends/clickhouse_lake.py
@@ -40,7 +40,7 @@ class XAPILakeClickhouse:
         }
 
         # For some reason get_client isn't automatically setting secure based on the port
-        # so we have to do it ourselves. This is obviously limiting, but should be 9% correct
+        # so we have to do it ourselves. This is obviously limiting, but should be 90% correct
         # and keeps us from adding yet another command line option.
         secure = str(self.port).endswith("443") or str(self.port).endswith("440")
 
@@ -248,7 +248,7 @@ class XAPILakeClickhouse:
             out_tag = f"""(
                 {tag["tag_id"]},
                 {tag["taxonomy_id"]},
-                {tag["parent_int_id"] or 'NULL'},
+                {tag["parent_int_id"] or 0},
                 '{tag["value"]}',
                 '{tag["id"]}',
                 '{tag["hierarchy"]}',
